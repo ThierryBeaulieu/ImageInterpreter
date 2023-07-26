6e9748b777475bb68695448b051d7f25db476f15
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-07-25 20:21:40 -0400

    httpParser: verification to see that we've got the content length

--- src/app/httpParser/httpParser.cpp
+++ src/app/httpParser/httpParser.cpp
@@ -3,9 +3,15 @@
 HttpParser::HttpParser(){}
 
 void HttpParser::parse(QByteArray httpRequest, const HttpContent& newContent){
+    if (!httpRequest.contains("Content-Length")){
+        qDebug() << "we return because we don't have content-elngth" << Qt::endl;
+        return;
+    }
     QVector<QByteArray> lines;
     QByteArray currentLine;
+    qDebug() << httpRequest << Qt::endl;
+
     for (auto i = httpRequest.cbegin(), end = httpRequest.cend(); i != end; ++i){
-        qDebug() << *i << Qt::endl;
+
     }
 }
