46532a97ea3740396ad8279516fc28c269275081
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-07-28 10:08:36 -0400

    httpserver: running app

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -8,15 +8,17 @@ int main(int argc, char *argv[])
     QCoreApplication app(argc, argv);
 
     QHttpServer httpServer;
-    // httpServer.route("/", []() {
-    //     return "Hello world";
-    // });
+    httpServer.route("/", []() {
+        return "Hello world";
+    });
 
-    // const auto port = httpServer.listen(QHostAddress::AnyIPv4);
-    // qDebug() << port << Qt::endl;
-    // if (!port) {
-    //     qWarning() << QCoreApplication::translate("QHttpServerExample",
-    //                                               "Server failed to listen on a port.");
-    //    return -1;
-    // }
-}
+    const auto port = httpServer.listen(QHostAddress::AnyIPv4);
+    qDebug() << port << Qt::endl;
+    if (!port) {
+        qWarning() << QCoreApplication::translate("QHttpServerExample",
+                                                   "Server failed to listen on a port.");
+        return -1;
+    }
+
+    return app.exec();
+} 
